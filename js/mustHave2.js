3998060701596f5d4278c1e99fa113ab64c271d8
dscheinin <dscheinin@gmail.com>
2018-04-05 19:04:20 -0300

    Material Design fixes

--- js/mustHave2.js
+++ js/mustHave2.js
@@ -13,24 +13,29 @@ function displayAllLocations() {
     };
 
     loadGeoJSON();
-    
-    map.data.setStyle(function(feature) {
+
+    map.data.setStyle(function (feature) {
         return {
             icon: image
         };
     });
 }
 
-function loadGeoJSON () {
+function loadGeoJSON() {
     //return await map.data.loadGeoJson('mustHave1.json');
-    fetch('mustHave1.json').then(function(features){
+    fetch('mustHave1.json').then(function (features) {
         return features.json();
-    }).then(function(jsonData){
+    }).then(function (jsonData) {
         map.data.addGeoJson(jsonData);
-        map.data.forEach(function(feature){
+        map.data.forEach(function (feature) {
             if (feature.getProperty('globantOffice') === '') {
-                var latlng = { lat: parseFloat(feature.getGeometry().get().lat()), lng: parseFloat(feature.getGeometry().get().lng()) };
-                geocoder.geocode({ 'location': latlng }, function(results, status) {
+                var latlng = {
+                    lat: parseFloat(feature.getGeometry().get().lat()),
+                    lng: parseFloat(feature.getGeometry().get().lng())
+                };
+                geocoder.geocode({
+                    'location': latlng
+                }, function (results, status) {
                     if (status === google.maps.GeocoderStatus.OK) {
                         if (results[0]) {
                             feature.setProperty('globantOffice',
@@ -44,23 +49,34 @@ function loadGeoJSON () {
 }
 
 function displayNearestLocations() {
-    var topNSelected = document.getElementById("topLocations") ? document.getElementById("topLocations").value : 1 ;
+    var topNSelected = document.getElementById("topLocations") ? document.getElementById("topLocations").value : 1;
     nearestLocationsPanel.innerHTML = "Nearest top " + topNSelected + " locations from current location:<br/>";
     var distanceToLocation = new Array();
-    map.data.forEach(function(feature) {
+    map.data.forEach(function (feature) {
         var distance = google.maps.geometry.spherical.computeDistanceBetween(currentPosition, feature.getGeometry().get()) / 1000;
-        distanceToLocation.push({ distance: distance, feature: feature.getGeometry().get(), name: feature.getProperty('globantOffice') });
+        distanceToLocation.push({
+            distance: distance,
+            feature: feature.getGeometry().get(),
+            name: feature.getProperty('globantOffice')
+        });
     });
     showNearestLocations(distanceToLocation);
 }
 
 function showNearestLocations(distanceToLocation) {
-    var topNSelected = document.getElementById("topLocations") ? document.getElementById("topLocations").value : 1 ;
-    distanceToLocation.sort(function(a, b) { return a.distance - b.distance });
+    var topNSelected = document.getElementById("topLocations") ? document.getElementById("topLocations").value : 1;
+    distanceToLocation.sort(function (a, b) {
+        return a.distance - b.distance
+    });
     nearestLocations = distanceToLocation.splice(0, distanceToLocation.length > topNSelected ? topNSelected : distanceToLocation.length);
-    nearestLocations.forEach(function(element) {
+    nearestLocations.forEach(function (element) {
         addStringToDivInnerHTML(nearestLocationsPanel,
-            "To " + element.name + " " +
-            element.distance.toFixed(2) + "km<br/>");
+            '<li class="mdl-list__item">' +
+            '<span class="mdl-list__item-primary-content">' +
+            '<i class="material-icons mdl-list__item-icon"></i>' +
+            'To ' + element.name + ' ' +
+            element.distance.toFixed(2) + 'km' +
+            '</span>' +
+            '</li>');
     });
 }
